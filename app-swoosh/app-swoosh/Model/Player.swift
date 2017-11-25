8eb008a24f0771a3039189c196ec0c4f968e41c4
Dariusz Czochara <dczochara@dccode.net>
2017-11-25 19:05:53 +0100

    l07

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Dariusz Czochara on 25.11.2017.
+//  Copyright © 2017 dccode.net. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
